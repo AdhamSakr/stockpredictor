0b31cea613008f7c6a155ee6f0774540024c05d9
Adham Sakr <ad.sakr@gmail.com>
2023-11-12 23:44:37 +0000

    Removed unnecessary commented code

--- etl/load_data.py
+++ etl/load_data.py
@@ -1,5 +1,3 @@
-import pandas as pd
-import sqlite3
 from sqlalchemy import create_engine
 
 
@@ -29,13 +27,6 @@ def load_dataframe_to_database(df):
                 "Close": "close",
             }
         )
-
-        # connection = sqlite3.connect("../database/data/sp500_db")
-        # # run query
-        # cursor = connection.cursor()
-        # sql = f"""INSERT INTO sp500 (date, symbol, open, high, low, adj_close, volume, basic_eps, net_income, total_revenue, total_expenses, total_debt, total_capitalization, total_assets, free_cash_flow, capital_expenditure, close) VALUES ()"""
-        # cursor.execute(sql)
-        # connection.commit()
 
         # Create a SQLite database engine
         database_path = "../database/data/sp500_db"
